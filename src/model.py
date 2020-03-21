347e4c871010bfce26eca53a686d0e3538764585
mounikaduddukuri <61065424+mounikaduddukuri@users.noreply.github.com>
2020-03-21 07:37:33 -0400

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,18 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 15 03:01:24 2020
-
-@author: SG0304944
-"""
-
-'''ResNet in PyTorch.
-
-For Pre-activation ResNet, see 'preact_resnet.py'.
-
-Reference:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Deep Residual Learning for Image Recognition. arXiv:1512.03385
-'''
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
